3b5dd4f43238d58032a01e0c27f3f8073d81eec8
shubhf18425 <shubh.aglf@gmail.com>
2025-04-22 15:26:54 +0530

    API-ERORR UTIILTY

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error { // Fixed class name
+  constructor(message = "Something went wrong", statusCode = 500, errors = [],
+    statck = ""
+  ) { // Added default value for statusCode
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors; // Fixed assignment of errors
+    if(statck){
+        this.statck = this.statck
+    }
+    else{
+        Error.captureStackTrace(this, this.constructor); // Fixed stack trace capture
+        
+    }
+
+}
+}
+
+export default ApiError; // Added export statement
